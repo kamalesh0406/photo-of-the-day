92d3770362fcea670991de637c0da99b3677de0f
Kamalesh Palanisamy <kamalesh800@gmail.com>
2019-06-09 15:30:40 +0530

    Feature Display Top Sites of User

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 $.get('https://www.nationalgeographic.com/photography/photo-of-the-day/',function(response){
-	console.log(response)
 	var picture = $(response).filter('meta[property="og:image"]').attr("content");
 	$('body').css('background', `url(${picture}) no-repeat center center fixed`);
 	$('body').css('background-size','cover');
+});
+chrome.topSites.get(function(data){
+	console.log(data);
+	var i = 0
+	data.forEach(function(website){
+		var favicon = website.url + "favicon.ico"
+		$("#sites").append(`<div class="col-md-6"><a class="btn" href="${website.url}" role="button"><img class="img-fluid" src="${favicon}""></img></a></div>`)
+	});
 })
